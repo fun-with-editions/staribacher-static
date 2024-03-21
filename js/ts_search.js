b965dfda3f8932388974f91a1c7fb4088cd2b71d
fsanzl <fsanzl@users.noreply.github.com>
2024-03-21 16:19:18 +0000

    deploy: 07bf2d10cebcd91d9fd245e3b8a0836ec53d3add

---
+++ js/ts_search.js
@@ -0,0 +1,154 @@
+var project_collection_name = "STB"
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: "JyGrjgl9YvrWJNIQp9a4qrUv85UZNZWiW5H9h9soa3wobRCm",
+    nodes: [
+      {
+        host: "localhost",
+        port: "8108",
+        protocol: "http",
+      },
+    ],
+  },
+  // The following parameters are directly passed to Typesense's search API endpoint.
+  //  So you can pass any parameters supported by the search endpoint below.
+  //  query_by is required.
+  //  filterBy is managed and overridden by InstantSearch.js. To set it, you want to use one of the filter widgets like refinementList or use the `configure` widget.
+  additionalSearchParameters: {
+    query_by: "full_text,title",
+  },
+});
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+  searchClient,
+  indexName: project_collection_name,
+});
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: "#searchbox",
+    autofocus: true,
+    cssClasses: {
+      form: "form-inline",
+      input: "form-control col-md-11",
+      submit: "btn",
+      reset: "btn",
+    },
+  }),
+
+  instantsearch.widgets.hits({
+    container: "#hits",
+    templates: {
+      empty: "Keine Resultate für <q>{{ query }}</q>",
+      item: `
+              <h5><a href="{{ anchor_link }}">{{#helpers.snippet}}{ "attribute": "title", "highlightedTagName": "mark" }{{/helpers.snippet}}</a></h5>
+              <p style="overflow:hidden;max-height:210px;">{{#helpers.snippet}}{ "attribute": "full_text", "highlightedTagName": "mark" }{{/helpers.snippet}}</p>
+              <h5><span class="badge badge-primary">{{ project }}</span></h5>
+          `,
+    },
+  }),
+
+  instantsearch.widgets.pagination({
+    container: "#pagination",
+  }),
+
+  instantsearch.widgets.stats({
+    container: "#stats-container",
+    templates: {
+      text: `
+            {{#areHitsSorted}}
+              {{#hasNoSortedResults}}keine Treffer{{/hasNoSortedResults}}
+              {{#hasOneSortedResults}}1 Treffer{{/hasOneSortedResults}}
+              {{#hasManySortedResults}}{{#helpers.formatNumber}}{{nbSortedHits}}{{/helpers.formatNumber}} Treffer {{/hasManySortedResults}}
+              aus {{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}}
+            {{/areHitsSorted}}
+            {{^areHitsSorted}}
+              {{#hasNoResults}}keine Treffer{{/hasNoResults}}
+              {{#hasOneResult}}1 Treffer{{/hasOneResult}}
+              {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} Treffer{{/hasManyResults}}
+            {{/areHitsSorted}}
+            gefunden in {{processingTimeMS}}ms
+          `,
+    },
+  }),
+
+  instantsearch.widgets.refinementList({
+    container: "#refinement-list-persons",
+    attribute: "persons",
+    searchable: true,
+    searchablePlaceholder: "Suchen",
+    cssClasses: {
+      searchableInput: "form-control form-control-sm mb-2 border-light-2",
+      searchableSubmit: "d-none",
+      searchableReset: "d-none",
+      showMore: "btn btn-secondary btn-sm align-content-center",
+      list: "list-unstyled",
+      count: "badge ml-2 badge-secondary hideme ",
+      label: "d-flex align-items-center text-capitalize",
+      checkbox: "mr-2",
+    },
+  }),
+
+  instantsearch.widgets.rangeSlider({
+    container: "#refinement-range-year",
+    attribute: "notbefore",
+    pips: false,
+    min: 9673200.0,
+    max: 422575200.0,
+    tooltips: {
+	    format: value => `${Date(value).toLocaleString("de-AT")}`,
+    },
+    cssClasses: {
+      form: "form-inline",
+      input: "form-control",
+      submit: "btn",
+    },
+  }),
+
+  instantsearch.widgets.pagination({
+    container: "#pagination",
+    padding: 2,
+    cssClasses: {
+      list: "pagination",
+      item: "page-item",
+      link: "page-link",
+    },
+  }),
+
+  instantsearch.widgets.clearRefinements({
+    container: "#clear-refinements",
+    templates: {
+      resetLabel: "Filter zurücksetzen",
+    },
+    cssClasses: {
+      button: "btn",
+    },
+  }),
+
+  instantsearch.widgets.currentRefinements({
+    container: "#current-refinements",
+    cssClasses: {
+      delete: "btn",
+      label: "badge",
+    },
+  }),
+
+  /*instantsearch.widgets.sortBy({
+    container: "#sort-by",
+    items: [
+      { label: "standard", value: `${project_collection_name}` },
+      { label: "chronologisch", value: `${project_collection_name}/sort/creation_date:asc, bv_doc_id_num:asc, doc_internal_orderval:asc` },
+      { label: "umgekehrt chronologisch", value: `${project_collection_name}/sort/creation_date:desc, bv_doc_id_num:asc, doc_internal_orderval:asc` },
+    ],
+  }),*/
+
+  instantsearch.widgets.configure({
+    hitsPerPage: 8,
+    attributesToSnippet: ["full_text"],
+    //filters: function() { return document.getElementsByTagName("input").value },
+  }),
+
+]);
+
+search.start();
